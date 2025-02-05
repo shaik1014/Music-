c9d0cef8d5d5011445af2f5ba55c14173c19abea
Beluga <cowork.co.in@gmail.com>
2025-02-05 11:15:31 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -2,10 +2,36 @@
 
 const menu = document.getElementById('menu');
 const buttons = document.getElementById('buttons');
+const button =document.querySelectorAll('.button');
 
 menu.addEventListener('click', () => {
     
-    buttons.classList.add('animate');
+    if(buttons.classList.contains('animate'))
+    {
+        buttons.classList.remove('animate');
+
+        button.forEach(button => {
+
+            if(button!==menu){
+
+                 button.classList.add('hidden');
+            }
+           
+        })
+    
+    }
+
+    else
+    {
+        buttons.classList.add('animate');
+
+        button.forEach(button => {
+            if(button!==menu){
+
+                button.classList.remove('hidden');
+            }
+        });
+    }
 
 
 });
