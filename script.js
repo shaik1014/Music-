5438400ac5dc07b8635017eab1a6dc339ee2d167
Beluga <cowork.co.in@gmail.com>
2025-04-13 22:36:11 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,17 +1,43 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const menu = document.getElementById("menu");
-    const buttons = document.getElementById("buttons");
-    const buttonElements = document.querySelectorAll(".button");
+
+
+
+
+
+
+
+
+
+
+  async function getSongs() {
+    let a = await fetch("http://127.0.0.1:5501/songs/");
+    let response = await a.text();
   
-    menu.addEventListener("click", () => {
-      buttons.classList.toggle("animate");
+    let div = document.createElement("div");
+    div.innerHTML = response;
   
-      buttonElements.forEach((button) => {
-        if (button !== menu) {
-          button.classList.toggle("hidden");
-        }
-      });
-    });
-  });
+    let as = div.getElementsByTagName("a");
+    let songs = [];
+  
+    for (let index = 0; index < as.length; index++) {
+      const element = as[index];
+      if (element.href.endsWith(".mp3")) {
+        songs.push(element.href);
+      }
+    }
+  
+    return songs;
+  }
+  
+  async function main() {
+    let songs = await getSongs();
+    console.log(songs);
+
+    var audio = new Audio(songs[0]);
+    audio.play();
+  }
+  
+  main();
+  
+
 
   
